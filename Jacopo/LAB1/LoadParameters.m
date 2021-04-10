00e61a23ea96d8204e7849acba84ed1b76999dca
Jacopo Pellizzari <jacopisky@gmail.com>
2021-04-10 10:31:51 +0200

    Add files via upload

--- Jacopo/LAB1/LoadParameters.m
+++ Jacopo/LAB1/LoadParameters.m
@@ -33,6 +33,33 @@ ss.controller.K = place(ss.plant.A,ss.plant.B,[p1 p2]);
 ss.obs.wc    = 2*pi*50;
 ss.obs.delta = 1/sqrt(2);
 
+sigma = -delta*wn;
+wd    = wn*sqrt(1-delta^2);
+opt1 = [sigma+1i*wd, sigma-1i*wd, sigma];
+opt2 = [sigma, sigma, sigma];
+opt3 = [2*sigma+1i*wd, 2*sigma-1i*wd, 2*sigma];
+opt4 = [2*sigma+1i*wd, 2*sigma-1i*wd, 3*sigma];
+A    = [0, ss.plant.C; [0;0], ss.plant.A];
+B    = [0; ss.plant.B];
+C    = [0, ss.plant.C];
+
+robust.controller1.K = place(A, B, opt1);
+robust.controller2.K = acker(A, B, opt2);
+robust.controller3.K = place(A, B, opt3);
+robust.controller4.K = place(A, B, opt4);
+
+robust.controller1.Ki = robust.controller1.K(1);
+robust.controller1.K  = robust.controller1.K(1,2:3);
+
+robust.controller2.Ki = robust.controller2.K(1);
+robust.controller2.K  = robust.controller2.K(1,2:3);
+
+robust.controller3.Ki = robust.controller3.K(1);
+robust.controller3.K  = robust.controller3.K(1,2:3);
+
+robust.controller4.Ki = robust.controller4.K(1);
+robust.controller4.K  = robust.controller4.K(1,2:3);
+
 clear a22;
 clear b2;
 clear c1;
@@ -41,3 +68,12 @@ clear wn;
 clear p1;
 clear p2;
 clear gains;
+clear sigma;
+clear wd;
+clear opt1;
+clear opt2;
+clear opt3;
+clear opt4;
+clear A;
+clear B;
+clear C;
