4fe4eb8182f409015e5ab598c28d74a8dc415e54
Jacopo Pellizzari <jacopisky@gmail.com>
2021-04-15 22:56:48 +0200

    Delete Jacopo directory

--- Jacopo/LAB1/LoadEstimatedParamsToPlant.m
+++
@@ -1,3 +0,0 @@
-mech.Jeq = est_par.J_eq;
-mech.Beq = est_par.B_eq;
-mld.tausf = est_par.tau_sf;
--- Jacopo/LAB1/data/Check.m
+++
@@ -1,6 +0,0 @@
-load("SSnominal120.mat");
-
-t = thl_meas.time;
-s =  thl_meas.signals.values;
-
-plot(t, s)
